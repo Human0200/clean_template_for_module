39ba32f59d70813bc73eb376340ebd5c363f2299
Anton <ruant02@mail.ru>
2025-08-05 16:41:05 +0500

    changes

---
+++ classes/settings.php
@@ -0,0 +1,15 @@
+<?php
+namespace Settings24;
+
+class GlobalSettings
+{
+    public static function getId()
+    {
+        return 'integrationbx24';
+    }
+    
+    public static function getPath()
+    {
+        return __DIR__;
+    }
+}
