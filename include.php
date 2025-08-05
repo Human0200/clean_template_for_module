f7527469495a6e1e5a5f9667dcc5ecaccdea01da
Anton <ruant02@mail.ru>
2025-08-05 16:38:29 +0500

    changes

--- include.php
+++ include.php
@@ -3,7 +3,7 @@
 use Bitrix\Main\Loader;
 
 Loader::registerAutoLoadClasses('leadspace.parsercsv', [
-    'Settings24\GlobalSettings' => 'lib/Classes/Settings.php',
+    'Settings24\GlobalSettings' => 'lib/classes/settings.php',
 
 ]);
 ?>
