10e583db7ef35acf457239036985afc0887ee0f2
Anton <ruant02@mail.ru>
2025-08-28 18:21:34 +0500

    changes

--- admin/menu.php
+++ admin/menu.php
@@ -1,12 +1,13 @@
 <?php
 
 use Bitrix\Main\Localization\Loc;
+use Settings24\GlobalSettings;
 
 AddEventHandler('main', 'OnBuildGlobalMenu', 'SupportMenu');
 
 function SupportMenu(&$arGlobalMenu, &$arModuleMenu)
 {
-    $moduleId = 'leadspace.parsercsv';
+    $moduleId = GlobalSettings::getId();
 
     if ($GLOBALS['APPLICATION']->GetGroupRight($moduleId) < 'R') {
         return;
@@ -15,8 +16,8 @@ function SupportMenu(&$arGlobalMenu, &$arModuleMenu)
     // Создаем корневой раздел "Интеграция" без подпунктов
     $arGlobalMenu['support_connection'] = [
         'menu_id' => 'global_menu_support',
-        'text' => Loc::getMessage('PARSER_CONNECTION_TITLE'),
-        'title' => Loc::getMessage('PARSER_CONNECTION_DESC'),
+        'text' => Loc::getMessage('_CONNECTION_TITLE'),
+        'title' => Loc::getMessage('_CONNECTION_DESC'),
         'sort' => 1000, // Позиция в меню
         'items_id' => 'global_menu_support_items',
         'icon' => 'update_menu_icon',
@@ -24,7 +25,7 @@ function SupportMenu(&$arGlobalMenu, &$arModuleMenu)
 
         $arGlobalMenu['support_connection']['items'][$moduleId] = [
         'menu_id' => 'menu_support_bx24',
-        'text' => Loc::getMessage('PARSER_MENU_TITLE'),
+        'text' => Loc::getMessage('_MENU_TITLE'),
         'items_id' => 'menu_support_items',
         'url' => '/bitrix/admin/module_connector_admin.php?lang='.LANGUAGE_ID,
         'icon' => 'catalog_menu_icon',
